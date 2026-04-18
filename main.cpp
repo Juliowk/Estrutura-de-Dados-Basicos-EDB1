e399f978fa52a731b7aaf0487b15a234ce2ef924
Julio <julioeliasdesousarocha.wk@gmail.com>
2026-04-18 19:12:10 -0300

    Torre de Hanoi

--- main.cpp
+++ main.cpp
@@ -5,12 +5,14 @@
 using namespace std;
 
 int main(int argc, char const* argv[]) {
-  
-  int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-  int lastElement = (sizeof(array) / sizeof(array[0])) - 1;
+    int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int lastElement = (sizeof(array) / sizeof(array[0])) - 1;
 
-  invert(array, 0, lastElement);
-  for (int number : array) cout << number << " ";
+    invert(array, 0, lastElement);
+    for (int number : array) cout << number << " ";
+    cout << endl;
 
-  return 0;
+    torreHanoi(5, 1, 2, 3);
+
+    return 0;
 }
--- recursion.cpp
+++ recursion.cpp
@@ -1,20 +1,32 @@
-#include <iostream>
-
 #include "recursion.h"
+
+#include <iostream>
 
 using namespace std;
 
 void swap(int* array, int a, int b) {
-  int temp = array[a];
-  array[a] = array[b];
-  array[b] = temp;
+    int temp = array[a];
+    array[a] = array[b];
+    array[b] = temp;
 }
 
 void invert(int* array, int initial, int end) {
-  if (initial >= end)
-    return;
-  else {
-    swap(array, initial, end);
-    invert(array, ++initial, --end);
-  }
+    if (initial >= end)
+        return;
+    else {
+        swap(array, initial, end);
+        invert(array, ++initial, --end);
+    }
+}
+
+void torreHanoi(int n, int source, int auxiliary, int target) {
+    if (n == 1) {
+        cout << "Mover disco da torre " << source << " para a torre " << target
+             << endl;
+        return;
+    }
+    torreHanoi(n - 1, source, target, auxiliary);
+    cout << "Mover disco da torre " << source << " para a torre " << target
+         << endl;
+    torreHanoi(n - 1, auxiliary, source, target);
 }
