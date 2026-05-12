12b85dc49caf7ba09282311ebb5d093fb487e317
Julio Rocha <julioeliasdesousarocha.work@gmail.com>
2026-05-12 10:13:39 -0300

    Add method Inserir

--- conjunto-array-main/src/Conjunto.cpp
+++ conjunto-array-main/src/Conjunto.cpp
@@ -2,22 +2,28 @@
 
 #include <iostream>
 
-Conjunto::Conjunto(const int capacidade): capacidade(capacidade), quantidade(0) {
-    this->elementos = new int[capacidade];
+Conjunto::Conjunto(const int capacidade)
+    : capacidade(capacidade), quantidade(0) {
+  this->elementos = new int[capacidade];
 }
 
-Conjunto::~Conjunto() {
-    delete[] this->elementos;
-}
+Conjunto::~Conjunto() { delete[] this->elementos; }
 
 bool Conjunto::contem(int elemento) const {
-    for (int i = 0; i < quantidade; i++)
-        if (this->elementos[i] == elemento) return true;
+  for (int i = 0; i < this->quantidade; i++)
+    if (this->elementos[i] == elemento) return true;
 
-    return false;
+  return false;
 }
 
-bool Conjunto::inserir(int elemento) { return elemento != 0; }
+bool Conjunto::inserir(int elemento) {
+  if (this->capacidade == this->quantidade) return false;
+  if (contem(elemento)) return false;
+
+  this->elementos[this->quantidade] = elemento;
+  this->quantidade++;
+  return true;
+}
 
 bool Conjunto::remover(int elemento) { return elemento != 0; }
 
