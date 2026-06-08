b303263d153fa2533b69c52d852a6ce672f58e00
Júlio Elias de Sousa rocha <julioeliasdesousarocha.work@gmail.com>
2026-06-08 18:04:41 +0000

    Add answer to q2

--- provas/un2/praticas/2024_2/q2/src/ListaEncadeada.cpp
+++ provas/un2/praticas/2024_2/q2/src/ListaEncadeada.cpp
@@ -20,14 +20,45 @@ ListaEncadeada::~ListaEncadeada()
     // To-Do
 }
 
-void ListaEncadeada::inserirNoInicio(int e){auto n = new No<int>(e);if( inicio == nullptr ) inicio = n;else { n->setProximo( inicio );inicio = n;}}
-
-int ListaEncadeada::removerTodos(int val) 
+void ListaEncadeada::inserirNoInicio(int e)
 {
-    throw "ERRO: 'removerTodos' ainda não foi implementado.";
+    auto n = new No<int>(e);
+    if (inicio == nullptr)
+        inicio = n;
+    else
+    {
+        n->setProximo(inicio);
+        inicio = n;
+    }
 }
 
-No<int>* ListaEncadeada::getInicio()
+int ListaEncadeada::removerTodos(int val)
+{
+    auto elemento = this->inicio;
+
+    int count = 0;
+
+    while (elemento != nullptr) {
+        
+        if (elemento == this->inicio && elemento->valor == val) {
+            this->inicio = elemento->proximo;
+            count++;
+        } else if (elemento == this->inicio && elemento->proximo != nullptr && elemento->proximo->valor == val) {
+            elemento->proximo = elemento->proximo->proximo;
+            count++;
+        } else if ( elemento->proximo != nullptr && elemento->proximo->valor == val) {
+            elemento->proximo = elemento->proximo->proximo;
+            count++;
+        }
+
+        elemento = elemento->proximo;
+    }
+
+    return count;
+    
+}
+
+No<int> *ListaEncadeada::getInicio()
 {
     return this->inicio;
 }
@@ -35,16 +66,15 @@ No<int>* ListaEncadeada::getInicio()
 std::string ListaEncadeada::imprimir()
 {
     std::stringstream resultado;
-    
+
     auto no = this->getInicio();
 
-    while(no != nullptr)
+    while (no != nullptr)
     {
-        resultado << no->getValor() << "->" ;
+        resultado << no->getValor() << "->";
         no = no->getProximo();
     }
     resultado << "null";
 
     return resultado.str();
 }
-
