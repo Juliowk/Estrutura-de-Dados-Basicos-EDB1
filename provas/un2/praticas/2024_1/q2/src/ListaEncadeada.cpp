720f52d1c0912cee04aca4ed7967dac7cbd8b2f5
Júlio Elias de Sousa rocha <julioeliasdesousarocha.work@gmail.com>
2026-06-09 16:51:52 +0000

    Add answer to q2

--- provas/un2/praticas/2024_1/q2/src/ListaEncadeada.cpp
+++ provas/un2/praticas/2024_1/q2/src/ListaEncadeada.cpp
@@ -13,6 +13,7 @@
 ListaEncadeada::ListaEncadeada()
 {
     this->inicio = nullptr;
+    this->quantidade = 0;
 }
 
 ListaEncadeada::~ListaEncadeada()
@@ -20,14 +21,48 @@ ListaEncadeada::~ListaEncadeada()
     // To-Do
 }
 
-void ListaEncadeada::inserirNoInicio(int e){auto n = new No<int>(e);if( inicio == nullptr ) inicio = n;else { n->setProximo( inicio );inicio = n;}}
-
-int ListaEncadeada::removerTodos(int val) 
+void ListaEncadeada::inserirNoInicio(int e)
 {
-    throw "ERRO: não implementou";
+    auto n = new No<int>(e);
+    if (inicio == nullptr)
+        inicio = n;
+    else
+    {
+        n->setProximo(inicio);
+        inicio = n;
+    }
+    this->quantidade++;
 }
 
-No<int>* ListaEncadeada::getInicio()
+int ListaEncadeada::removerTodos(int val) {
+    auto atual = this->inicio;
+    No<int>* anterior = nullptr;
+
+    int count = 0;
+
+    while (atual != nullptr) {
+
+        auto proximo = atual->getProximo();
+
+        if (atual->getValor() == val) {
+            if (atual == this->inicio)
+                this->inicio = atual->getProximo();
+            else
+                anterior->setProximo(proximo);
+
+            count++;
+            delete atual;
+        } else{
+            anterior = atual;
+        }
+
+        atual = proximo;
+    }
+
+    return count;
+}
+
+No<int> *ListaEncadeada::getInicio()
 {
     return this->inicio;
 }
@@ -35,16 +70,15 @@ No<int>* ListaEncadeada::getInicio()
 std::string ListaEncadeada::imprimir()
 {
     std::stringstream resultado;
-    
+
     auto no = this->getInicio();
 
-    while(no != nullptr)
+    while (no != nullptr)
     {
-        resultado << no->getValor() << "->" ;
+        resultado << no->getValor() << "->";
         no = no->getProximo();
     }
     resultado << "null";
 
     return resultado.str();
 }
-
